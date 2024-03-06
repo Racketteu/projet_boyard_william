67ee032cf8dacc7c05ff0602f8936b04877b7638
Boyard <williamboyard1@gmail.com>
2024-03-06 22:38:40 +0100

    TP2.8

--- tp02/frontend/src/app/app.component.ts
+++ tp02/frontend/src/app/app.component.ts
@@ -2,14 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { ClientFormComponent } from './client-form/client-form.component';
-import { DataSummaryComponent } from './data-summary/data-summary.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,ClientFormComponent,DataSummaryComponent],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent,],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- tp02/frontend/src/app/app.routes.ts
+++ tp02/frontend/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { AccueilComponent } from './accueil/accueil.component';
+import { SellComponent } from './sell/sell.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: 'form', component: FormComponent},
+    {path: 'accueil', component: AccueilComponent},
+    {path: 'sell', component: SellComponent},
+];
