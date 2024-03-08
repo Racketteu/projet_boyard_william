85d2f24b50a592ae61fdc1ceaae7eda7353b16a0
Boyard <williamboyard1@gmail.com>
2024-03-08 23:02:16 +0100

    Barre de recherche

--- tp02/frontend/src/app/liste/liste.component.ts
+++ tp02/frontend/src/app/liste/liste.component.ts
@@ -3,24 +3,35 @@ import { Observable } from 'rxjs';
 import { ApiproductService } from '../apiproduct.service';
 import { CoffeeProduct } from '../model/CoffeeProduct';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-liste',
   templateUrl: './liste.component.html',
   styleUrls: ['./liste.component.css'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true
 })
 
 export class ListeComponent implements OnInit {
-
   coffeeProducts!: Observable<CoffeeProduct[]>;
+  filteredProducts: CoffeeProduct[] = [];
 
   constructor(private apiService: ApiproductService) { }
 
   ngOnInit() {
     this.coffeeProducts = this.apiService.getCoffeeProduct();
-    console.log(this.coffeeProducts)
+    this.coffeeProducts.subscribe(products => {
+      this.filteredProducts = products;
+    });
   }
+
+  filterProducts(event: any) {
+    const searchText: string = event.target.value;
+    this.coffeeProducts.subscribe(products => {
+      this.filteredProducts = products.filter(product =>
+        product.name.toLowerCase().includes(searchText.toLowerCase())
+      );
+    });
+  }  
 }
