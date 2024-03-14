379b2ba7bc5e0ea29f389d887234f38c02161ad5
Boyard <williamboyard1@gmail.com>
2024-03-14 17:00:50 +0100

    TP04

--- tp02/frontend/src/app/app.component.ts
+++ tp02/frontend/src/app/app.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent} from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
+import { CarteModule } from './carte/carte.module';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent,FooterComponent,],
+  imports: [RouterOutlet,HeaderComponent,FooterComponent, CarteModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-
 }
--- tp02/frontend/src/app/app.routes.ts
+++ tp02/frontend/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { FormComponent } from './form/form.component';
 import { AccueilComponent } from './accueil/accueil.component';
 import { ListeComponent } from './liste/liste.component';
+import { PaiementComponent } from './carte/paiement/paiement.component';
 
 
 export const routes: Routes = [
     {path: 'form', component: FormComponent},
     {path: 'accueil', component: AccueilComponent},
     {path: 'liste', component: ListeComponent},
+    {path: 'paiement', component: PaiementComponent}
 ];
